216653469dedb6ab27b942c0bd8ad1737296c66c
your_username <web.shepel@gmail.com>
2022-04-07 13:05:07 +0300

    update packages & add editorconfig, prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [require("prettier-plugin-organize-attributes")],
+    "attributeGroups": ["$CODE_GUIDE"],
+    tabWidth: 4,
+    printWidth: 120,
+    bracketSameLine: true,
+    htmlWhitespaceSensitivity: 'ignore'
+}
