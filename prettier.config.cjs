216af5f6d79858895051e9ddee74a7dfa7e4e513
your_username <web.shepel@gmail.com>
2023-01-19 11:31:45 +0200

    updated config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,9 @@
 module.exports = {
-    plugins: [require("prettier-plugin-organize-attributes")],
-    "attributeGroups": ["$CODE_GUIDE"],
+    plugins: [require('prettier-plugin-organize-attributes')],
+    attributeGroups: ['$CODE_GUIDE'],
     tabWidth: 4,
     printWidth: 120,
     bracketSameLine: true,
-    htmlWhitespaceSensitivity: 'ignore'
-}
+    htmlWhitespaceSensitivity: 'ignore',
+    singleQuote: true,
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,26 @@
-import { createPopper } from "@popperjs/core";
-import { Modal } from "bootstrap";
-import IMask from "imask";
+import { createPopper } from '@popperjs/core';
+import { Modal } from 'bootstrap';
+import IMask from 'imask';
 
 // Get real vh
 
 (function () {
     const setRealHeight = () => {
         const doc = document.documentElement;
-        doc.style.setProperty("--vh", `${window.innerHeight}px`);
+        doc.style.setProperty('--vh', `${window.innerHeight}px`);
     };
-    window.addEventListener("resize", setRealHeight);
+    window.addEventListener('resize', setRealHeight);
     setRealHeight();
 })();
 
 // Phone mask
 
 (function () {
-    const phoneInputs = document.querySelectorAll("input[type=tel]");
+    const phoneInputs = document.querySelectorAll('input[type=tel]');
 
     phoneInputs.forEach((el) => {
         IMask(el, {
-            mask: "+{38} (000) 000-00-00",
+            mask: '+{38} (000) 000-00-00',
         });
     });
 })();
